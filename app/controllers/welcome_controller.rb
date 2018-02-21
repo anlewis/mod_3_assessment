c60c52e5f98ad726eb7123dac952ad60784bc569
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-21 08:31:04 -0700

    Practice faraday call in welcome index; commented out for now

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    # @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1") do |faraday|
+    #   faraday.headers["app_id"] = ENV["oxford_app_id"]
+    #   faraday.headers["app_key"] = ENV["oxford_app_key"]
+    #   faraday.headers["Accept"] = "application/json"
+    #   faraday.adapter Faraday.default_adapter
+    # end
+
+    # response = @conn.get("entries/en/ace/synonyms")
+
+    # @synonyms = JSON.parse(response.body, symbolize_names: true)
   end
 end
