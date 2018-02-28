0c82cc54a1b2e171b12696dcdce516dff6712836
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-27 22:07:46 -0700

    Add invalid message json response for api play creation

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -2,15 +2,11 @@ class Api::V1::PlaysController < ApplicationController
   def create
     word = params[:play][:word]
 
-    OxfordService.new.validate_word(word)
-
-
-
-    play = Play.new(user_id: params[:play][:user_id],
+    if OxfordService.new.validate_word(word)
+      Play.create(user_id: params[:play][:user_id],
                 game: Game.find(params[:game_id]),
                 word: params[:play][:word]
                )
-    if play.save
       render json: {status: 201}.to_json
     else
       render json: {message: "foxez is not a valid word."}.to_json
