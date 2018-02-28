0af38abc2bea44c92c04dfaba6e85760d1afc5c6
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-27 20:51:50 -0700

    Refactor controller logic into service

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -3,19 +3,9 @@ class PlaysController < ApplicationController
 
   def create
     word = params[:word]
+    root = OxfordService.new.make_play(word)
 
-    # extract into service poro
-    @conn = Faraday.new(url: "https://od-api.oxforddictionaries.com/api/v1") do |faraday|
-      faraday.headers["app_id"] = ENV["oxford_app_id"]
-      faraday.headers["app_key"] = ENV["oxford_app_key"]
-      faraday.headers["Accept"] = "application/json"
-      faraday.adapter Faraday.default_adapter
-    end
-
-    response = @conn.get("inflections/en/#{word}")
-
-    if valid_word?(response)
-      root = find_root(response)
+    if root
       Play.create(word: params[:word])
       flash[:success] = "'#{word}' is a valid word and its root form is '#{root}'."
     else
@@ -24,17 +14,4 @@ class PlaysController < ApplicationController
 
     redirect_to root_url
   end
-
-  private
-
-    def valid_word?(response)
-      response.status == 200
-    end
-
-    def find_root(response)
-      JSON.parse(response.body, symbolize_names: true)[:results]
-        .first[:lexicalEntries]
-        .first[:inflectionOf]
-        .first[:text]
-    end
 end
