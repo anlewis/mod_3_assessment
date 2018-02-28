297645f6579a18fbf09c9e44d3d70ff7221bb00c
Anna Lewis <anna.noel.lewis@gmail.com>
2018-02-27 23:08:07 -0700

    Minor cleanup

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -6,9 +6,9 @@ class Api::V1::PlaysController < ApplicationController
 
     if OxfordService.new.validate_word(word)
       Play.create(user_id: params[:user_id],
-                game: Game.find(params[:game_id]),
-                word: word
-               )
+                  game: Game.find(params[:game_id]),
+                  word: word
+                 )
       render json: {message: "you've played the word: #{word}!"}.to_json
     else
       render json: {message: "#{word} is not a valid word."}.to_json
